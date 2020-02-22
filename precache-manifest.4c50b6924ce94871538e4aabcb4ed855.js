fa231e8365294183ebf57a99cf81897d54b88e0e
Jardelmc <jardel.casteluber@gmail.com>
2020-02-22 00:30:45 -0300

    Updates

---
+++ precache-manifest.4c50b6924ce94871538e4aabcb4ed855.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7b37afe7cdb5f67646c82057c27b73",
+    "url": "/nosso-saldo-frontend/index.html"
+  },
+  {
+    "revision": "c3669e1bd6bbb90e4d2c",
+    "url": "/nosso-saldo-frontend/static/css/2.750837fa.chunk.css"
+  },
+  {
+    "revision": "c3669e1bd6bbb90e4d2c",
+    "url": "/nosso-saldo-frontend/static/js/2.61508d54.chunk.js"
+  },
+  {
+    "revision": "48a0835985d74656fcb1",
+    "url": "/nosso-saldo-frontend/static/js/main.ec51e1f7.chunk.js"
+  },
+  {
+    "revision": "79edb11c94fd47bc28a1",
+    "url": "/nosso-saldo-frontend/static/js/runtime~main.ad52aea3.js"
+  }
+]);
