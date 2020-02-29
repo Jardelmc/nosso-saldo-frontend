80c456a2ff1e55708f6b39210ccfae8990970b6f
Jardelmc <jardel.casteluber@gmail.com>
2020-02-29 19:06:09 -0300

    Updates

---
+++ precache-manifest.f1a7cb941bf6308164a47f32abcfde45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d75c9cc3df347d72ff35712cddc2efb",
+    "url": "/nosso-saldo-frontend/index.html"
+  },
+  {
+    "revision": "9d24b0d02fdcf26d1224",
+    "url": "/nosso-saldo-frontend/static/css/2.e73af137.chunk.css"
+  },
+  {
+    "revision": "9d24b0d02fdcf26d1224",
+    "url": "/nosso-saldo-frontend/static/js/2.0d3dc03d.chunk.js"
+  },
+  {
+    "revision": "3b5a20d359f85e3f06b2",
+    "url": "/nosso-saldo-frontend/static/js/main.ec5a6c27.chunk.js"
+  },
+  {
+    "revision": "79edb11c94fd47bc28a1",
+    "url": "/nosso-saldo-frontend/static/js/runtime~main.ad52aea3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nosso-saldo-frontend/precache-manifest.7c40bb99fafbb21e45fea935af89823c.js"
+  "/nosso-saldo-frontend/precache-manifest.f1a7cb941bf6308164a47f32abcfde45.js"
 );
 
 self.addEventListener('message', (event) => {
