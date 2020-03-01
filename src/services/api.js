e34b59497d5989094c9a56e04d5f40937a222a87
Jardelmc <jardel.casteluber@gmail.com>
2020-03-01 17:50:26 -0300

    fix - server url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const serverUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://68.183.111.160'
+    ? 'https://api.nossosaldo.life'
     : 'http://192.168.3.110:3900';
 
 const api = axios.create({
