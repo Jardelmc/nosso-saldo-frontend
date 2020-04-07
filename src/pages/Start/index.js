6d898e0afba9a3463204c02c8a9a1c603aa2bbbe
Jardelmc <jardel.casteluber@gmail.com>
2020-04-06 23:43:14 -0300

    fix - show two decimal in balance

--- src/pages/Start/index.js
+++ src/pages/Start/index.js
@@ -146,19 +146,7 @@ export default function Start() {
       return `${value},00`;
     }
     if (value) {
-      value = String(value);
-      value = value.replace('.', ',');
-      const splittedValue = value.split(',');
-
-      if (splittedValue.length > 1) {
-        const valueReturn =
-          splittedValue[1].length === 2
-            ? `${splittedValue[0]},${splittedValue[1]}`
-            : `${splittedValue[0]},${splittedValue[1]}0`;
-
-        return valueReturn;
-      }
-      return `${value},00`;
+      return value.toFixed(2).replace('.', ',');
     }
 
     return false;
